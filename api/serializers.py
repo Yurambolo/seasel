6a5fdb93618fed77dfca34beab38ff29395a77fa
Yurambolo <61801166+Yurambolo@users.noreply.github.com>
2021-11-28 22:52:02 +0200

    fixed serializers

--- api/serializers.py
+++ api/serializers.py
@@ -9,12 +9,16 @@ class MusicSchoolSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class UserSerializer(serializers.HyperlinkedModelSerializer):
+    music_school = MusicSchoolSerializer(many=False, read_only=True)
+
     class Meta:
         model = User
         fields = ['email', 'name', 'music_school', 'role']
 
 
 class SemesterSerializer(serializers.HyperlinkedModelSerializer):
+    music_school = MusicSchoolSerializer(many=False, read_only=True)
+
     class Meta:
         model = Semester
         fields = ['music_school', 'number', 'composition_count', 'max_difficulty', 'min_difficulty']
@@ -27,36 +31,55 @@ class InstrumentSerializer(serializers.HyperlinkedModelSerializer):
 
 
 class ConcertSerializer(serializers.HyperlinkedModelSerializer):
+    music_school = MusicSchoolSerializer(many=False, read_only=True)
+
     class Meta:
         model = Concert
         fields = ['music_school', 'date']
 
 
 class CompositionSerializer(serializers.HyperlinkedModelSerializer):
+    instrument = InstrumentSerializer(many=False, read_only=True)
+
     class Meta:
         model = Composition
         fields = ['name', 'instrument', 'author', 'difficulty']
 
 
 class CompositionRepresentationSerializer(serializers.HyperlinkedModelSerializer):
+    composition = CompositionSerializer(many=False, read_only=True)
+
     class Meta:
         model = CompositionRepresentation
         fields = ['composition', 'format', 'sheme']
 
 
 class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    student = UserSerializer(many=False, read_only=True)
+    teacher = UserSerializer(many=False, read_only=True)
+    semester = SemesterSerializer(many=False, read_only=True)
+    instrument = InstrumentSerializer(many=False, read_only=True)
+
     class Meta:
         model = Course
         fields = ['student', 'teacher', 'semester', 'instrument']
 
 
 class ProgramSerializer(serializers.HyperlinkedModelSerializer):
+    concert = ConcertSerializer(many=False, read_only=True)
+    course = CourseSerializer(many=False, read_only=True)
+    semester = SemesterSerializer(many=False, read_only=True)
+    compositions = CompositionSerializer(many=True, read_only=True)
+
     class Meta:
         model = Program
         fields = ['concert', 'course', 'semester', 'compositions']
 
 
 class RepetitionSerializer(serializers.HyperlinkedModelSerializer):
+    course = CourseSerializer(many=False, read_only=True)
+    composition = CompositionSerializer(many=False, read_only=True)
+
     class Meta:
         model = Repetition
         fields = ['course', 'composition', 'datetime', 'mark']
